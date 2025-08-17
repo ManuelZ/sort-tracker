e0208b505553ad6b45aa3098c5d4d2f78bd75ce0
ManuelZ <115771+ManuelZ@users.noreply.github.com>
2025-08-17 12:25:05 -0500

    Cleaning (auto formatting)

--- sort.py
+++ sort.py
@@ -35,9 +35,7 @@ where:
 
 The state transition model
 The velocities and the aspect ratio stay constant over time (with some process noise). 
-
 """
-
 
 
 class KFTracker:
@@ -57,15 +55,14 @@ class KFTracker:
         self.update_streak = 0
 
         # Added because when a misdetection ocurrs, update_streak resets to zero and the tracked object isn't displayed
-        # anymore, although the tracker is still alive. With this variable and an updated rule, the tracker box is 
+        # anymore, although the tracker is still alive. With this variable and an updated rule, the tracker box is
         # shown even if misdetections happen and only dissapears when the tracker is removed.
         self.updates = 0
-
 
     def create_filter(self, bbox):
         """
         State:
-            x = [u, v, s, r, u_dot, v_dot, s_dot].T 
+            x = [u, v, s, r, u_dot, v_dot, s_dot].T
 
         Motion model:
             u[t] = u[t-1] + u_dot * dt
@@ -79,39 +76,49 @@ class KFTracker:
 
         # State transition model that predicts the new state
         state_transition_model = np.array(
-            [[1, 0, 0, 0, 1, 0, 0],
-             [0, 1, 0, 0, 0, 1, 0],
-             [0, 0, 1, 0, 0, 0, 1],
-             [0, 0, 0, 1, 0, 0, 0],
-             [0, 0, 0, 0, 1, 0, 0],
-             [0, 0, 0, 0, 0, 1, 0],
-             [0, 0, 0, 0, 0, 0, 1]
-            ], dtype=np.float64
+            [
+                [1, 0, 0, 0, 1, 0, 0],
+                [0, 1, 0, 0, 0, 1, 0],
+                [0, 0, 1, 0, 0, 0, 1],
+                [0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 1],
+            ],
+            dtype=np.float64,
         )
-        
-        process_noise_covariance = np.diag([10, 10, 10, 10, 1e4, 1e4, 1e4]).astype(np.float64)
-        
+
+        process_noise_covariance = np.diag([10, 10, 10, 10, 1e4, 1e4, 1e4]).astype(
+            np.float64
+        )
+
         # No Control model
         control_model = None
 
         # Measurement matrix, aka Observation matrix. Only u, v, s and r are being measured.
         observation_matrix = np.array(
-            [[1, 0, 0, 0, 0, 0, 0],
-             [0, 1, 0, 0, 0, 0, 0],
-             [0, 0, 1, 0, 0, 0, 0],
-             [0, 0, 0, 1, 0, 0, 0]
-            ], dtype=np.float64
+            [
+                [1, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0],
+            ],
+            dtype=np.float64,
         )
 
         measurement_noise_covariance = np.diag([1, 1, 1, 1]).astype(np.float64)
-        
-        # Create the initial state with zero velocities
-        initial_state = np.vstack((
-            self.__bbox_to_state(bbox),
-            np.array([0, 0, 0], dtype=np.float64).reshape(-1, 1)
-        ))
 
-        initial_state_covariance = np.diag([1, 1, 1, 1, 1e-1, 1e-1, 1e-1]).astype(np.float64)
+        # Create the initial state with zero velocities
+        initial_state = np.vstack(
+            (
+                self.__bbox_to_state(bbox),
+                np.array([0, 0, 0], dtype=np.float64).reshape(-1, 1),
+            )
+        )
+
+        initial_state_covariance = np.diag([1, 1, 1, 1, 1e-1, 1e-1, 1e-1]).astype(
+            np.float64
+        )
 
         return kalman.Kalman(
             F=state_transition_model,
@@ -120,12 +127,12 @@ class KFTracker:
             Q=process_noise_covariance,
             R=measurement_noise_covariance,
             x_prev=initial_state,
-            P_prev=initial_state_covariance
+            P_prev=initial_state_covariance,
         )
 
     def __bbox_to_state(self, bbox, dtype=np.float64):
         """
-        Convert a bounding box defined by its top-left and bottom-right corners [x1, y1, x2, y2] to an observation 
+        Convert a bounding box defined by its top-left and bottom-right corners [x1, y1, x2, y2] to an observation
         vector [x, y, s, r], where:
 
         - x, y : float
@@ -187,7 +194,7 @@ class KFTracker:
             self.update_streak = 0
         self.cycles_since_update += 1
         return self.__state_to_bbox(prior)
-    
+
     def update(self, bbox):
         """ """
         self.cycles_since_update = 0
@@ -199,11 +206,16 @@ class KFTracker:
 
 
 class SortTracker:
-    def __init__(self, iou_threshold=0.3, max_cycles_without_update=2, min_hits=3, starting_id=1):
-        """
-        """
-        
-        self.trackers:list[KFTracker] = []
+    def __init__(
+        self,
+        iou_threshold=0.3,
+        max_cycles_without_update=1,
+        min_updates=3,
+        starting_id=1,
+    ):
+        """ """
+
+        self.trackers: list[KFTracker] = []
         self._id_counter = count(starting_id)
         self.iou_threshold = iou_threshold
         self.min_updates = min_updates
@@ -211,7 +223,7 @@ class SortTracker:
         self.frame_count = 0
 
     def get_next_id(self):
-        """ Return a sequential integer."""
+        """Return a sequential integer."""
         return next(self._id_counter)
 
     def associate_detected_and_predicted_boxes(
@@ -222,19 +234,19 @@ class SortTracker:
         ) -> Tuple[np.ndarray, np.ndarray]:
         """
         Associate detection and tracking boxes based on their IoU values.
-        
-        - In assigning detections to existing targets, each target's bounding box geometry is estimated by predicting 
-          its new location in the current frame. 
-        - The assignment cost matrix is then computed as the intersection-over-union (IOU) distance between each 
-          detection and all predicted bounding boxes from the existing targets. 
-        - The assignment is solved optimally using the Hungarian algorithm. 
+
+        - In assigning detections to existing targets, each target's bounding box geometry is estimated by predicting
+          its new location in the current frame.
+        - The assignment cost matrix is then computed as the intersection-over-union (IOU) distance between each
+          detection and all predicted bounding boxes from the existing targets.
+        - The assignment is solved optimally using the Hungarian algorithm.
 
         Description from paper: "Simple Online And Realtime Tracking"
         """
         # TODO: improve the cost matrix
         iou_matrix = self._calculate_cost_matrix(detections, predictions)
-        
-        if len(iou_matrix) > 0:        
+
+        if len(iou_matrix) > 0:
             # (row_idx, col_idx) pairs that identify the prediction that corresponds to a detection
             detection_indices, prediction_indices = linear_sum_assignment(-iou_matrix)
         else:
@@ -251,16 +263,16 @@ class SortTracker:
         Description from paper: "Simple Online And Realtime Tracking"
         """
 
-        matches: list[tuple[int,int]] = []
+        matches: list[tuple[int, int]] = []
         unmatched_detections: list[int] = []
-        #unmatched_predictions: list[int] = []
+        # unmatched_predictions: list[int] = []
         for detection_idx, prediction_idx in zip(detection_indices, prediction_indices):
             if iou_matrix[detection_idx, prediction_idx] >= self.iou_threshold:
                 matches.append((detection_idx, prediction_idx))
             else:
                 unmatched_detections.append(detection_idx)
-                #unmatched_predictions.append(prediction_idx)
-        
+                # unmatched_predictions.append(prediction_idx)
+
         # List detections without a matching track box. A  new tracker will be created for them.
         for detection_idx, detection in enumerate(detections):
             if detection_idx not in detection_indices:
@@ -275,9 +287,9 @@ class SortTracker:
     def _get_bbox_priors(self) -> list[list[float]]:
         """
         Compute and return a list of predicted bounding boxes, excluding any predictions with NaN values.
-        
+
         This function iterates over all trackers and performs the prediction step of the Kalman filter for each.
-        During the Kalman filter update, numerical issues can cause the state variable representing the bounding box 
+        During the Kalman filter update, numerical issues can cause the state variable representing the bounding box
         size (e.g., in a state vector [u, v, s, r, ...]) to become negative. This leads to the calculated bounding boxes
         to be formed by NaN values.
         This may occur when the error between the prediction and the measurement (the innovation) is large—such as when
@@ -290,16 +302,16 @@ class SortTracker:
                       ^^^
                       size variable
 
-        The size can become negative during the calculation of the posterior, when `y` is negative and `K` is big, 
+        The size can become negative during the calculation of the posterior, when `y` is negative and `K` is big,
         `K @ y` can become a big number that makes the `size` variable negative.
 
             # This is the error between the prediction and measurement
             self.y = z - self.H @ self.x_prior
-            
+
             # Update the estimate with measurements from sensor.
             self.x_posterior = self.x_prior + K @ self.y
 
-        This scenario is typically observed when a bounding box shrinks excessively due to a tracked object leaving the 
+        This scenario is typically observed when a bounding box shrinks excessively due to a tracked object leaving the
         visible frame.
 
         """
@@ -321,7 +333,7 @@ class SortTracker:
             else:
                 print(f"Removing dead tracker with id '{tracker.id}'")
         return alive_trackers
-    
+
     def create_trackers(self, detections, unmatched_detections):
         """ """
         trackers = []
@@ -331,46 +343,49 @@ class SortTracker:
             trackers.append(new_tracker)
             print(f"New tracker created with id '{new_id}'")
         return trackers
-        
+
     def should_output_tracker(self, tracker):
         """
-        Determine whether a given tracker should be output based on its update status, hit streak, and the current 
-        frame count.        
+        Determine whether a given tracker should be output based on its update status, hit streak, and the current
+        frame count.
         """
-        
+
         # Check if the tracker was updated in the current cycle.
         # In the original code, this is `tracker.cycles_since_update < 1`
-        tracker_updated_this_cycle = tracker.cycles_since_update < self.max_cycles_without_update
-        
+        tracker_updated_this_cycle = (
+            tracker.cycles_since_update < self.max_cycles_without_update
+        )
+
         # Check if the tracker has accumulated enough hits to be considered reliable.
         # In the original code, this is `update_streak > self.min_updates`
         has_sufficient_updates = tracker.updates >= self.min_updates
-        
+
         # Check if we're in the early frames where lower hit streaks are acceptable.
         is_in_initial_phase = self.frame_count <= self.min_updates
-        
-        # The tracker should be output if it was updated this cycle and either has a sufficient hit streak or is in the 
-        # initial phase.
-        return tracker_updated_this_cycle and (has_sufficient_updates or is_in_initial_phase)
 
+        # The tracker should be output if it was updated this cycle and either has a sufficient hit streak or is in the
+        # initial phase.
+        return tracker_updated_this_cycle and (
+            has_sufficient_updates or is_in_initial_phase
+        )
 
     def update(self, detections):
         """
-        - The tracker is initialised using the geometry of the bounding box with the velocity set to zero. 
-        - Since the velocity is unobserved at this point the covariance of the velocity component is initialised with 
-          large values, reflecting this uncertainty. 
-        - Additionally, the new tracker then undergoes a probationary period where the target needs to be associated 
+        - The tracker is initialised using the geometry of the bounding box with the velocity set to zero.
+        - Since the velocity is unobserved at this point the covariance of the velocity component is initialised with
+          large values, reflecting this uncertainty.
+        - Additionally, the new tracker then undergoes a probationary period where the target needs to be associated
           with detections to accumulate enough evidence in order to prevent tracking of false positives.
 
-        - Tracks are terminated if they are not detected for TLost frames. This prevents an unbounded growth in the 
-          number of trackers and localisation errors caused by predictions over long durations without corrections from 
-          the detector. 
-        
+        - Tracks are terminated if they are not detected for TLost frames. This prevents an unbounded growth in the
+          number of trackers and localisation errors caused by predictions over long durations without corrections from
+          the detector.
+
         - In all experiments TLost is set to 1 for two reasons:
            - Firstly, the constant velocity model is a poor predictor of the true dynamics
-           - Secondly we are primarily concerned with frame-to-frame tracking where object re-identification is beyond 
-             the scope of this work. 
-          Additionally, early deletion of lost targets aids efficiency. Should an object reappear, tracking will 
+           - Secondly we are primarily concerned with frame-to-frame tracking where object re-identification is beyond
+             the scope of this work.
+          Additionally, early deletion of lost targets aids efficiency. Should an object reappear, tracking will
           implicitly resume under a new identity.
 
         Description from paper: "Simple Online And Realtime Tracking"
@@ -386,24 +401,25 @@ class SortTracker:
         for detection_idx, predicted_idx in matches:
             self.trackers[predicted_idx].update(detections[detection_idx])
 
-        # Create new trackers        
+        # Create new trackers
         new_trackers = self.create_trackers(detections, unmatched_detections)
         self.trackers.extend(new_trackers)
 
         # Remove trackers that have not been updated in a while
         self.trackers = self.filter_dead_trackers()
-        
+
         results = []
         for tracker in self.trackers:
             if self.should_output_tracker(tracker):
-                results.append({"predicted_bbox": tracker.posterior_bbox, "id": tracker.id})
+                results.append(
+                    {"predicted_bbox": tracker.posterior_bbox, "id": tracker.id}
+                )
 
         return results
 
     @staticmethod
     def _calculate_cost_matrix(detections, predictions, dtype=np.float64):
-        """
-        """
+        """ """
         # TODO: This double loop looks costly. The original implementation vectorizes it, but is hard to read.
         iou_matrix = []
         for det in detections:
@@ -430,7 +446,7 @@ def calculate_iou(boxA: np.ndarray, boxB: np.ndarray) -> float:
     # compute the area of both the prediction and ground-truth rectangles
     boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
     boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-    # compute the intersection over union by taking the intersection area and dividing it by the sum of 
+    # compute the intersection over union by taking the intersection area and dividing it by the sum of
     # prediction + ground-truth areas - the intersection area
     iou = interArea / float(boxAArea + boxBArea - interArea)
     # return the intersection over union value
@@ -454,36 +470,35 @@ def draw_tracking_box(image, observation):
     """
 
     left, top, right, bottom = map(int, observation["predicted_bbox"])
-    observation_id = observation["id"] 
-    
+    observation_id = observation["id"]
+
     image = cv2.rectangle(
-        img=image, 
+        img=image,
         pt1=(left, top),
         pt2=(right, bottom),
-        color=id_to_color(observation_id*10),
-        thickness=3
+        color=id_to_color(observation_id * 10),
+        thickness=3,
     )
-    
+
     image = cv2.putText(
         img=image,
         text=str(observation_id),
         org=(left - 10, top - 10),
         fontFace=cv2.FONT_HERSHEY_SIMPLEX,
         fontScale=1,
-        color=id_to_color(observation_id*10),
-        thickness=3
+        color=id_to_color(observation_id * 10),
+        thickness=3,
     )
     return image
 
 
 if __name__ == "__main__":
-    
     COMPARE_WITH_YOLO_TRACK = True
     SELECTED_CLASSES = ["car"]
     video_filename = "MOT16-13-raw.mp4"  # https://motchallenge.net/data/MOT16/
     sort_tracker = SortTracker(max_cycles_without_update=3)
     model = YOLO("yolo11l.pt")
-    
+
     class_names = list(model.names.values())
     selected_indices = [class_names.index(option) for option in SELECTED_CLASSES]
 
@@ -493,9 +508,9 @@ if __name__ == "__main__":
         show=COMPARE_WITH_YOLO_TRACK,
         classes=selected_indices,
         stream=True,
-        verbose=False
+        verbose=False,
     )
-    
+
     for r in results:
         boxes = [box.xyxy[0].cpu().numpy() for box in r.boxes]
         results = sort_tracker.update(boxes)
